88e270b3c24de555e591d8607b4db4f66b766c2e
Matthias Stieger <matthiass413@gmail.com>
2017-04-17 13:28:30 +0200

    Ansteuerung für Stepper hinzugefügt

--- KletterHandlingRoboter/Debug/KletterHandlingRoboter.cpp.d
+++ KletterHandlingRoboter/Debug/KletterHandlingRoboter.cpp.d
@@ -11,4 +11,10 @@ C:\Users\Matthias\AppData\Local\Temp\VMBuilds\KletterHandlingRoboter\mega_atmega
  D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/USBAPI.h \
  D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
  D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\variants\mega/pins_arduino.h \
- C:\Users\Matthias\AppData\Local\Temp\VMBuilds\KletterHandlingRoboter\mega_atmega2560\Debug\TrackSensor.h
+ C:\Users\Matthias\AppData\Local\Temp\VMBuilds\KletterHandlingRoboter\mega_atmega2560\Debug\TrackSensor.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\libraries\Wire\src/Wire.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Stream.h \
+ D:\Users\Matthias\Documents\Arduino\libraries\Adafruit_Motor_Shield_V2_Library-master/Adafruit_MotorShield.h \
+ D:\Users\Matthias\Documents\Arduino\libraries\Adafruit_Motor_Shield_V2_Library-master/utility/Adafruit_MS_PWMServoDriver.h \
+ D:\Program\ Files\ (x86)\Arduino\hardware\arduino\avr\cores\arduino/Arduino.h \
+ D:\Users\Matthias\Documents\Arduino\libraries\Adafruit_Motor_Shield_V2_Library-master/utility/Adafruit_MS_PWMServoDriver.h
--- KletterHandlingRoboter/KletterHandlingRoboter.ino
+++ KletterHandlingRoboter/KletterHandlingRoboter.ino
@@ -5,13 +5,19 @@
 */
 
 #include "TrackSensor.h"
+#include <Wire.h>
+#include <Adafruit_MotorShield.h>
+#include "utility/Adafruit_MS_PWMServoDriver.h"
 
+Adafruit_MotorShield AFMS = Adafruit_MotorShield(0x60);
+Adafruit_StepperMotor *Stepper1 = AFMS.getStepper(200, 1);
 TrackSensor Sensor1(0);
 TrackSensor Sensor2(1);
 
 // the setup function runs once when you press reset or power the board
 void setup() {
 	Serial.begin(9600);
+	AFMS.begin();
 }
 
 // the loop function runs over and over again until power down or reset
@@ -22,4 +28,14 @@ void loop() {
 	Serial.print("Sensor2 = ");
 	Serial.println(Sensor2.result());
 	delay(1000);
+	if (! Sensor1.result()){
+		stepper(1000, 1000, FORWARD, DOUBLE);		
+	}
+}
+
+
+// stepper controll
+void stepper(int speed, int stepps, uint8_t direction, uint8_t steptype) {
+	Stepper1->setSpeed(speed);
+	Stepper1->step(stepps, direction, steptype);
 }
